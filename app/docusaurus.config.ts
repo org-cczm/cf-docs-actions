b598f64e9f47b681a1df60a792aa5c37f2c7402f
yxw <yxw@debian128.cczm.org>
2024-11-17 12:51:37 +0800

    modified version
    修改版本号

--- app/docusaurus.config.ts
+++ app/docusaurus.config.ts
@@ -131,7 +131,7 @@ const config: Config = {
           ],
         },
       ],
-      copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
+      copyright: `Copyright © ${new Date().getFullYear()} 9M2DESKTOP Project, Inc. Built with Docusaurus.`,
     },
     prism: {
       theme: prismThemes.github,
