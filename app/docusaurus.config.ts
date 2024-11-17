502a8194a989180b827e23678b59f79ee9a81fff
org-cczm <dev@cczm.org>
2024-11-17 09:22:29 +0800

    change title

--- app/docusaurus.config.ts
+++ app/docusaurus.config.ts
@@ -5,8 +5,8 @@ import type * as Preset from '@docusaurus/preset-classic';
 // This runs in Node.js - Don't use client-side code here (browser APIs, JSX...)
 
 const config: Config = {
-  title: 'My Site',
-  tagline: 'Dinosaurs are cool',
+  title: 'My Site@CF',
+  tagline: 'Dinosaurs with Cloudflare',
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
@@ -68,7 +68,7 @@ const config: Config = {
     // Replace with your project's social card
     image: 'img/docusaurus-social-card.jpg',
     navbar: {
-      title: 'My Site',
+      title: 'My Site@CF',
       logo: {
         alt: 'My Site Logo',
         src: 'img/logo.svg',
